9ba5658430232e762d0b556945bbdd6451f224dc
Candace Van Doren <cvd2586@gmail.com>
2019-09-10 21:33:13 +0000

    Deleted portion of code in quotes_controller.rb to fix a forbidden attributes error

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -30,40 +30,6 @@ class QuotesController < ApplicationController
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
     end
     redirect_to root_path
-
-    respond_to do |format|
-      if @quote.save
-        format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
-        format.json { render :show, status: :created, location: @quote }
-      else
-        format.html { render :new }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /quotes/1
-  # PATCH/PUT /quotes/1.json
-  def update
-    respond_to do |format|
-      if @quote.update(quote_params)
-        format.html { redirect_to @quote, notice: 'Quote was successfully updated.' }
-        format.json { render :show, status: :ok, location: @quote }
-      else
-        format.html { render :edit }
-        format.json { render json: @quote.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /quotes/1
-  # DELETE /quotes/1.json
-  def destroy
-    @quote.destroy
-    respond_to do |format|
-      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
-      format.json { head :no_content }
-    end
   end
 
   private
--- app/models/Quotes.rb
+++
@@ -1,3 +0,0 @@
-class Quote < ApplicationRecord
-
-end
