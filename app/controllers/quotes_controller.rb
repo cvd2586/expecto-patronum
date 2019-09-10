40b694c0e3ee141ccf1f09b075580f9112eef548
Candace Van Doren <cvd2586@gmail.com>
2019-09-10 22:16:57 +0000

    Built the about page

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -32,6 +32,8 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_quote
